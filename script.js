3e477acf1ce726a2ee8367df3c70c2c30f9f57fa
mokapot <nganhtu04597@gmail.com>
2023-07-08 19:28:01 +0700

    Refactor the code to using class

--- script.js
+++ script.js
@@ -1,89 +1,178 @@
 //DOM selectors
-const booksList = document.querySelector('.books-list');
-const bookName = document.querySelector('#name');
-const bookAuthor = document.querySelector('#author');
-const bookStatus = document.querySelector('#status');
-const submitBtn = document.querySelector('#submit');
+// const booksList = document.querySelector('.books-list');
+// const bookName = document.querySelector('#name');
+// const bookAuthor = document.querySelector('#author');
+// const bookStatus = document.querySelector('#status');
+// const submitBtn = document.querySelector('#submit');
 
-// library array
-let myLibrary = [];
+// // library array
+// let myLibrary = [];
 
-//Book obj constructor
-function Book (name, author, status) {
-    this.name = name
-    this.author = author
-    this.status = status
-}
+// //Book obj constructor
+// function Book (name, author, status) {
+//     this.name = name
+//     this.author = author
+//     this.status = status
+// }
 
-//create default data and add them to the library array
-const defaltData = [
-    {name : "The Lord of The Ring",
-    author : "J.R.R Tolkien",
-    status: "Read"}
-];
-defaltData.forEach(element => {
-    myLibrary.push(element);
-});
+// //create default data and add them to the library array
+// const defaltData = [
+//     {name : "The Lord of The Ring",
+//     author : "J.R.R Tolkien",
+//     status: "Read"}
+// ];
+// defaltData.forEach(element => {
+//     myLibrary.push(element);
+// });
 
-//functions
-function displayBooks(book, index) {
-    //create name div
-    let nameDiv = document.createElement('div');
-    nameDiv.textContent = book.name;
-    //create author div
-    let authorDiv = document.createElement('div');
-    authorDiv.textContent = book.author;
-    //create status div
-    let statusDiv = document.createElement('div');
-    statusDiv.textContent = book.status;
-    //create unique delete button with id = index and click event
-    let delBtn = document.createElement('button');
-    delBtn.classList.add('delete-btn');
-    delBtn.textContent = "DELETE";
-    delBtn.id = index;
-    delBtn.addEventListener('click', function() {
-        myLibrary.splice(delBtn.id, 1);
-        emptyLibrary();
-        showLibrary();
-    });
-    //create book-row div
-    let bookRowDiv = document.createElement('div');
-    bookRowDiv.classList.add('book-row');
-    bookRowDiv.appendChild(nameDiv);
-    bookRowDiv.appendChild(authorDiv);
-    bookRowDiv.appendChild(statusDiv);
-    bookRowDiv.appendChild(delBtn);
-    //add book-row to books-list
-    booksList.appendChild(bookRowDiv);
-}
+// //functions
+// function displayBooks(book, index) {
+//     //create name div
+//     let nameDiv = document.createElement('div');
+//     nameDiv.textContent = book.name;
+//     //create author div
+//     let authorDiv = document.createElement('div');
+//     authorDiv.textContent = book.author;
+//     //create status div
+//     let statusDiv = document.createElement('div');
+//     statusDiv.textContent = book.status;
+//     //create unique delete button with id = index and click event
+//     let delBtn = document.createElement('button');
+//     delBtn.classList.add('delete-btn');
+//     delBtn.textContent = "DELETE";
+//     delBtn.id = index;
+//     delBtn.addEventListener('click', function() {
+//         myLibrary.splice(delBtn.id, 1);
+//         emptyLibrary();
+//         showLibrary();
+//     });
+//     //create book-row div
+//     let bookRowDiv = document.createElement('div');
+//     bookRowDiv.classList.add('book-row');
+//     bookRowDiv.appendChild(nameDiv);
+//     bookRowDiv.appendChild(authorDiv);
+//     bookRowDiv.appendChild(statusDiv);
+//     bookRowDiv.appendChild(delBtn);
+//     //add book-row to books-list
+//     booksList.appendChild(bookRowDiv);
+// }
 
-//loop through myLibrary and display each element
-function showLibrary () {
-    myLibrary.forEach(element => {
-        displayBooks(element, myLibrary.indexOf(element));
-    });
-}
+// //loop through myLibrary and display each element
+// function showLibrary () {
+//     myLibrary.forEach(element => {
+//         displayBooks(element, myLibrary.indexOf(element));
+//     });
+// }
 
-//empty the books-list div
-function emptyLibrary() {
-    while (booksList.firstChild) {
-        booksList.removeChild(booksList.lastChild);
+// //empty the books-list div
+// function emptyLibrary() {
+//     while (booksList.firstChild) {
+//         booksList.removeChild(booksList.lastChild);
+//     }
+// }
+
+// // main script ===================================================
+
+// showLibrary();
+
+// submitBtn.addEventListener('click', addBookToLibrary);
+
+// function addBookToLibrary() {
+//     event.preventDefault();
+//     let bookStatusTxt = "Read";
+//     if(bookStatus.value === "0") bookStatusTxt = "Not read"; 
+//     book = new Book(bookName.value, bookAuthor.value, bookStatusTxt);
+//     myLibrary.push(book);
+
+//     emptyLibrary();
+//     showLibrary();
+// }
+
+class Book {
+    constructor(name, author, status) {
+        this.name = name;
+        this.author = author;
+        this.status = status;
+    }
+
+    getName() {
+        return this.name;
+    }
+    getAuthor() {
+        return this.author;
+    }
+    getStatus() {
+        return this.status;
     }
 }
 
-// main script ===================================================
+function screenController() {
+    let myLibrary = [];
+    let defaltData = new Book("The Lord of The Ring","J.R.R Tolkien","Read");
+    myLibrary.push(defaltData);
 
-showLibrary();
+    const booksList = document.querySelector('.books-list');
+    const bookName = document.querySelector('#name');
+    const bookAuthor = document.querySelector('#author');
+    const bookStatus = document.querySelector('#status');
+    const submitBtn = document.querySelector('#submit');
 
-submitBtn.addEventListener('click', addBookToLibrary);
+    submitBtn.addEventListener('click', addBookToLibrary);
 
-function addBookToLibrary() {
-    event.preventDefault();
-    let bookStatusTxt = "Read";
-    if(bookStatus.value === "0") bookStatusTxt = "Not read"; 
-    book = new Book(bookName.value, bookAuthor.value, bookStatusTxt);
-    myLibrary.push(book);
+    function addBookToLibrary() {
+        event.preventDefault();
+        let bookStatusTxt = "Read";
+        if(bookStatus.value === "0") bookStatusTxt = "Not read"; 
+        newBook = new Book(bookName.value, bookAuthor.value, bookStatusTxt);
+        myLibrary.push(newBook);
+    
+        emptyLibrary();
+        showLibrary();
+    }
 
-    emptyLibrary();
+    function displayBooks(book, index) {
+        //create name div
+        let nameDiv = document.createElement('div');
+        nameDiv.textContent = book.name;
+        //create author div
+        let authorDiv = document.createElement('div');
+        authorDiv.textContent = book.author;
+        //create status div
+        let statusDiv = document.createElement('div');
+        statusDiv.textContent = book.status;
+        //create unique delete button with id = index and click event
+        let delBtn = document.createElement('button');
+        delBtn.classList.add('delete-btn');
+        delBtn.textContent = "DELETE";
+        delBtn.id = index;
+        delBtn.addEventListener('click', function() {
+            myLibrary.splice(delBtn.id, 1);
+            emptyLibrary();
+            showLibrary();
+        });
+        //create book-row div
+        let bookRowDiv = document.createElement('div');
+        bookRowDiv.classList.add('book-row');
+        bookRowDiv.appendChild(nameDiv);
+        bookRowDiv.appendChild(authorDiv);
+        bookRowDiv.appendChild(statusDiv);
+        bookRowDiv.appendChild(delBtn);
+        //add book-row to books-list
+        booksList.appendChild(bookRowDiv);
+    }
+
+    function emptyLibrary() {
+        while (booksList.firstChild) {
+            booksList.removeChild(booksList.lastChild);
+        }
+    }
+
+    function showLibrary () {
+        myLibrary.forEach(element => {
+            displayBooks(element, myLibrary.indexOf(element));
+        });
+    }
     showLibrary();
 }
+
+screenController();
